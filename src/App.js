b18372cd4c564171af9c00756e9d05042e891246
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2022-12-04 10:41:16 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import image from "./Image/image.jpg"
+import { CgProfile } from 'react-icons/cg';
+import { RiListSettingsLine } from 'react-icons/ri';
 import './App.css';
 
 function App() {
   return (
     <div >
-     <img className="w-full h-[200px] object-cover" src={image}  />
-     <h1 className="sant">hello sant marti</h1>
+     <header>
+      <RiListSettingsLine/>
+       <CgProfile/>
+      
+     </header>
+    
     </div>
   );
 }
