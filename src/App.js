a2afeb0db1e5eb5d7cbac3090861bda57fa9e922
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2022-12-04 10:30:10 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import image from "./Image/image.jpg"
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <img className="w-full h-[200px] object-cover" src={image}  />
+     <h1 className="sant">hello sant marti</h1>
+    </div>
+  );
+}
+
+export default App;
