c2409aa3282c8ac463d57f4834193663d62e1285
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2022-12-04 12:43:30 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { CgProfile } from 'react-icons/cg';
 import { RiListSettingsLine } from 'react-icons/ri';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ElParque from './components/ElParque/ElParque';
+import ElBarrio from './components/ElBarro/ElBarro';
+import Entidades from './components/Entidades/Entidades';
+import Agenda from './components/Agenda/Agends';
 
 import './App.css';
 
 function App() {
-  // const slideLeft = () => {
-  //   var slider = document.getElementById('slider')
-  //   slider.scrollLeft = slider.scrollLeft -500
-  // }
 
-  // const slideRight = () => {
-  //   var slider = document.getElementById('slider')
-  //   slider.scrollLeft = slider.scrollLeft + 500
-  // }
+
   return (
+  <>
+ <Router>
     <div>
      <header className='header'>
       <RiListSettingsLine/>
@@ -25,13 +25,65 @@ function App() {
       <h1 className='titlle'>La Martinense</h1>
      </div>
      <div className='containerSlide'>   
-      <button>El Parque</button>
-      <button>Historia</button>
-      <button> Agenda</button>
-      <button> Entitas</button>
+      <Link
+        to="/"
+        type="submit"
+      >El Parque</Link>
+      <Link 
+       to="/historia"
+       type="submit"
+       >Historia</Link>
+      <Link 
+       to="/agenda"
+       type="submit"
+       > Agenda</Link>
+      <Link  
+        to="/entidades"
+        type="submit"
+        > Entidades</Link>
      </div>
+      <Link  
+        to="/barrio"
+        type="submit"
+        > El Barrio</Link>
+   
     </div>
+    
+    <Routes>
+      <Route 
+      exact
+      path='/'
+      element={
+        <ElParque/>
+      }
+      />
+          <Route 
+      exact
+      path='/'
+      element={
+        <ElBarrio/>
+      }
+      />
+          <Route 
+      exact
+      path='/'
+      element={
+        <Entidades/>
+      }
+      />
+          <Route 
+      exact
+      path='/'
+      element={
+        <Agenda/>
+      }
+      />
+    </Routes>
+    </Router>
+    
+  </>
   );
+  
 }
 
 export default App;
---
+++ src/components/Agenda/Agends.js
@@ -0,0 +1,13 @@
+const Agenda = () => {
+    return (
+        <div>
+            <div>
+            <h3>El barrio eres tu</h3>
+            <p>¡Participa de tu barrio! La Martinense quiere ser un espacio para todes, por lo que si tienes una idea para un evento que te gustaría que ocurriese en el barrio, ¡háznoslo saber! Pulsa aquí.
+</p>
+</div>
+<div></div>
+        </div>
+    )
+}
+export default Agenda
